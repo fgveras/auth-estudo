f46e188b0bc1224bd5a4c0591a251fcdc349bb93
Fernando Veras <nandogomescode@gmail.com>
2026-09-18 22:40:11 -0300

    adds-crud

---
+++ 2-finnas-back/FinnasAPI/FinnasAPI/Controllers/APITestController.cs
@@ -0,0 +1,65 @@
+﻿using FinnasDbService;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace FinnasAPI.Controllers 
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class APITestController : ControllerBase
+    {
+        [HttpGet("CreateCredor")]
+        public IActionResult CreateCredor()
+        {
+            var service = new CredoresDbService();
+
+            int outParam = service.CreateOrUpdate(new CredoresRecord
+            {
+                Id = 0,
+                IsActive = true,
+                NomeCredor = "Test Credor backend 2",
+                IsPessoaFisica = false,
+                IsRecorrente = false,
+                IsInstFin = false,
+                CodigoInstFin = "",
+                DhCriacao = DateTime.Now,
+                DhAtualizacao = DateTime.Now
+            });
+
+            return Ok(outParam);
+        }
+
+        [HttpGet("UpdateCredor")]
+        public IActionResult UpdateCredor()
+        {
+            var service = new CredoresDbService();
+
+            int outParam = service.CreateOrUpdate(new CredoresRecord
+            {
+                Id = 8,
+                IsActive = true,
+                NomeCredor = "Teste Update Backend",
+                IsPessoaFisica = false,
+                IsRecorrente = false,
+                IsInstFin = false,
+                CodigoInstFin = null,
+                DhCriacao = DateTime.Now,
+                DhAtualizacao = DateTime.Now
+            });
+
+            return Ok(outParam);
+        }
+
+        [HttpGet("GetCreador")]
+        public IActionResult GetCredor([FromQuery] int id)
+        {
+            var service = new CredoresDbService();
+
+            var record = service.Get(id);
+
+            var teste = JsonSerializer.Serialize(record);
+
+            return Ok(teste);
+        }
+    }
+}
--- 2-finnas-back/FinnasDbService/FinnasDbService/CredoresDbService.cs
+++ 2-finnas-back/FinnasDbService/FinnasDbService/CredoresDbService.cs
@@ -29,13 +29,13 @@ namespace FinnasDbService
 		public bool IsInstFin  { get; set; }
 		
 		[DataMember]
-		public string CodigoInstFin  { get; set; }
+		public string? CodigoInstFin  { get; set; }
 		
 		[DataMember]
-		public DateTime DhCriacao  { get; set; }
+		public DateTime? DhCriacao  { get; set; }
 		
 		[DataMember]
-		public DateTime DhAtualizacao  { get; set; }
+		public DateTime? DhAtualizacao  { get; set; }
 
 		public CredoresRecord()
 		{
@@ -44,7 +44,8 @@ namespace FinnasDbService
 			IsPessoaFisica = false;
 			IsRecorrente = false;
 			IsInstFin = false;
-			DhCriacao = DateTime.MinValue;
+			CodigoInstFin = null;
+            DhCriacao = DateTime.MinValue;
 			DhAtualizacao = DateTime.MinValue;
 		}
     }
@@ -53,28 +54,74 @@ namespace FinnasDbService
 	{
 
 		private string _table = "Credores";
-
-        public CredoresRecord Get()
+		private string _connectionString = $@"Server=(localdb)\MSSQLLocalDB;Database=finnas_db2;Trusted_Connection=True;TrustServerCertificate=True;";
+		private string _command = string.Empty;
+		
+        public CredoresRecord Get(int id)
 		{
-			return new CredoresRecord();
+
+			_command = $@"SELECT * FROM Credores WHERE id = @id";
+
+
+            using (SqlConnection connection = new SqlConnection(this._connectionString))
+            using (SqlCommand command = new SqlCommand(_command, connection))
+			{
+				try
+				{
+					command.Parameters.Add("@Id", SqlDbType.Int).Value = id;
+
+                    connection.Open();
+
+					using (SqlDataReader reader = command.ExecuteReader())
+					{
+						while (reader.Read())
+						{
+							return new CredoresRecord()
+							{
+								Id = reader.GetInt32(0)
+								, IsActive = reader.GetBoolean(1)
+								, NomeCredor = reader.GetString(2)
+								, IsPessoaFisica = reader.GetBoolean(3)
+								, IsRecorrente = reader.GetBoolean(4)
+								, IsInstFin = reader.GetBoolean(5)
+								, CodigoInstFin = reader.GetString(6)
+								, DhCriacao = reader.GetDateTime(7)
+								, DhAtualizacao = reader.GetDateTime(8)
+							};
+
+						};
+					}
+                }
+				catch (Exception)
+				{
+
+					throw;
+				}
+			}
+
+            return new CredoresRecord();
         }
 
 		public int CreateOrUpdate(CredoresRecord record)
 		{
-			return record.Id == 0 ? Create(record) : Update();  
+			return record.Id == 0 ? Create(record) : Update(record);  
 		}
 
-		public void Delete()
+		public void Delete(int id)
 		{
+			this._command = $@"DELETE FROM {this._table} WHERE Id = @Id";
 
+			using(SqlConnection cnn = new SqlConnection(this._connectionString))
+			using(SqlCommand cmd = new SqlCommand(this._command, cnn))
+			{
+
+			}
 		}
 
 		private int Create(CredoresRecord record)
-		{
-            string connectioString = "Server=(localdb)\\MSSQLLocalDB;Database=finnas_db2;Trusted_Connection=True;TrustServerCertificate=True;";
-
-            string query = $@"
-				INSERT INTO {_table} (
+		{            
+            this._command = $@"
+				INSERT INTO {this._table} (
 					IsActive
 					, NomeCredor
 					, IsPessoaFisica
@@ -96,20 +143,20 @@ namespace FinnasDbService
 				)
 				SELECT SCOPE_IDENTITY();";
             
-            using (SqlConnection connection = new SqlConnection(connectioString))
-            using (SqlCommand command = new SqlCommand(query, connection))
+            using (SqlConnection connection = new SqlConnection(this._connectionString))
+            using (SqlCommand command = new SqlCommand(this._command, connection))
             {
-                command.Parameters.Add("@IsActive", SqlDbType.Bit).Value = record.IsActive;
-                command.Parameters.Add("@NomeCredor", SqlDbType.NVarChar, 100).Value = record.NomeCredor;
-                command.Parameters.Add("@IsPessoaFisica", SqlDbType.Bit).Value = record.IsPessoaFisica;
-                command.Parameters.Add("@IsRecorrente", SqlDbType.Bit).Value = record.IsRecorrente;
-                command.Parameters.Add("@IsInstFin", SqlDbType.Bit).Value = record.IsInstFin;
-                command.Parameters.Add("@CodigoInstFin", SqlDbType.NVarChar, 50).Value = record.CodigoInstFin;
-                command.Parameters.Add("@DhCriacao", SqlDbType.DateTime2).Value = record.DhCriacao;
-                command.Parameters.Add("@DhAtualizacao", SqlDbType.DateTime2).Value = record.DhAtualizacao;
-				
                 try
                 {             
+					command.Parameters.Add("@IsActive", SqlDbType.Bit).Value = record.IsActive;
+					command.Parameters.Add("@NomeCredor", SqlDbType.NVarChar, 100).Value = record.NomeCredor;
+					command.Parameters.Add("@IsPessoaFisica", SqlDbType.Bit).Value = record.IsPessoaFisica;
+					command.Parameters.Add("@IsRecorrente", SqlDbType.Bit).Value = record.IsRecorrente;
+					command.Parameters.Add("@IsInstFin", SqlDbType.Bit).Value = record.IsInstFin;
+					command.Parameters.Add("@CodigoInstFin", SqlDbType.NVarChar, 50).Value = record.CodigoInstFin;
+					command.Parameters.Add("@DhCriacao", SqlDbType.DateTime2).Value = record.DhCriacao;
+					command.Parameters.Add("@DhAtualizacao", SqlDbType.DateTime2).Value = record.DhAtualizacao;
+				
                     connection.Open();
                     object result = command.ExecuteScalar();                    
 
@@ -127,9 +174,46 @@ namespace FinnasDbService
             }            
 		}
 
-		private int Update()
+		private int Update(CredoresRecord record)
 		{
-            return 100;
+			_command = $@"
+				UPDATE CREDORES SET 
+					IsActive = @IsActive
+					, NomeCredor =  @NomeCredor
+					, IsPessoaFisica =  @IsPessoaFisica
+					, IsRecorrente =  @IsRecorrente
+					, IsInstFin =  @IsInstFin
+					, CodigoInstFin = @CodigoInstFin
+					, DhCriacao =  @DhCriacao
+					, DhAtualizacao =  @DhAtualizacao
+				WHERE
+					Id = @Id";
+
+            using (SqlConnection connection = new SqlConnection(this._connectionString))
+            using (SqlCommand command = new SqlCommand(_command, connection))
+            {
+                try
+                {
+					command.Parameters.Add("@Id", SqlDbType.Int).Value = record.Id;
+					command.Parameters.Add("@IsActive", SqlDbType.Bit).Value = record.IsActive;
+					command.Parameters.Add("@NomeCredor", SqlDbType.NVarChar, 100).Value = record.NomeCredor;
+					command.Parameters.Add("@IsPessoaFisica", SqlDbType.Bit).Value = record.IsPessoaFisica;
+					command.Parameters.Add("@IsRecorrente", SqlDbType.Bit).Value = record.IsRecorrente;
+					command.Parameters.Add("@IsInstFin", SqlDbType.Bit).Value = record.IsInstFin;
+					command.Parameters.Add("@CodigoInstFin", SqlDbType.NVarChar, 50).Value = record.CodigoInstFin;
+					command.Parameters.Add("@DhCriacao", SqlDbType.DateTime2).Value = record.DhCriacao;
+					command.Parameters.Add("@DhAtualizacao", SqlDbType.DateTime2).Value = record.DhAtualizacao;
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+
+					return 0;
+                }
+                catch (SqlException)
+                {
+                    throw;
+                }
+            }
         }
 
 		private void CreateParameter(string name, SqlDbType type, object value)
